3bb87df7b7c0fbcb64245570ac1a8506bd60ce9b
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-01 21:06:12 +0200

    reviewing spread & rest

---
+++ studies/spread_&_rest.js
@@ -0,0 +1,9 @@
+// SPREAD:
+//---------
+
+// spread on arrays:
+// (...spread) spread out the items of the array into individual components
+
+const sports = ["soccer", "swimming", "basketball"];
+console.log(...sports) // soccer swimming basketball
+
